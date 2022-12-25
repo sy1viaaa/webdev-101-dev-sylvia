a109963872fcdb72f220da4aed482b6352ae5856
Jay Joo <jj534@cornell.edu>
2022-12-25 14:44:11 +0900

    init repo

---
+++ src/lib/pocketbase.ts
@@ -0,0 +1,11 @@
+import PocketBase from 'pocketbase';
+
+import { writable } from 'svelte/store';
+
+export const pb = new PocketBase('https://svelte-pocket.fly.dev/');
+export const currentUser = writable(pb.authStore.model);
+
+pb.authStore.onChange((auth) => {
+	console.log('authStore changed', auth);
+	currentUser.set(pb.authStore.model);
+});
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+  import Login from '$lib/Login.svelte'
+	import Messages from '$lib/Messages.svelte';
+</script>
+
+
+<Login />
+<Messages />
+
