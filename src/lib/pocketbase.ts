728fdae3ced37408a506a25eba5f2e55c63a73d1
Jay Joo <jj534@cornell.edu>
2023-01-18 19:11:51 +0900

    remove pocketbase url

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -2,7 +2,7 @@ import PocketBase from 'pocketbase';
 
 import { writable } from 'svelte/store';
 
-export const pb = new PocketBase('https://svelte-pocket.fly.dev/');
+export const pb = new PocketBase('enter your pocketbase url here');
 export const currentUser = writable(pb.authStore.model);
 
 pb.authStore.onChange((auth) => {
