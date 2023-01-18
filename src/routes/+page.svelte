ae0b4ac1ba5666561f12e2238cd9b0355ebd64f9
Jay Joo <jj534@cornell.edu>
2023-01-18 19:26:41 +0900

    move create post button modal out of auth nav

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script>
+	import CreatePostButtonModal from '$lib/components/CreatePostButtonModal.svelte';
 	import Posts from '$lib/components/Posts.svelte';
 </script>
 
+<CreatePostButtonModal />
 <Posts />
