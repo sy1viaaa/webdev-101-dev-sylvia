b01982ab9751e77881c5c1709b8857184abcad41
Sylvia Han <sjh326@cornell.edu>
2023-02-02 18:11:06 -0500

    Integrate Backend

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -2,7 +2,7 @@ import PocketBase from 'pocketbase';
 
 import { writable } from 'svelte/store';
 
-export const pb = new PocketBase('enter your pocketbase url here');
+export const pb = new PocketBase('https://webdev-101-dev-sylvia.fly.dev/');
 export const currentUser = writable(pb.authStore.model);
 
 pb.authStore.onChange((auth) => {
